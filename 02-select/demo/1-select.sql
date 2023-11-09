1325258f763e4266e2399aeaf7dc5baef0110af6
groya52 <Groya52@gmail.com>
2023-11-09 22:50:02 +0300

    add select

---
+++ 02-select/demo/1-select.sql
@@ -0,0 +1,94 @@
+﻿--выбор базы
+use WideWorldImporters;
+
+--варианты обращения к таблице
+select * from WideWorldImporters.Application.Cities
+select * from Application.Cities
+--select * from Cities
+
+
+--* - все колонки
+-- (*) с осторожностью на больших таблицах, лучше указать нужные колонки
+-- сравним планы (Ctrl+M)
+select * from Sales.OrderLines
+select OrderID from Sales.OrderLines
+
+
+--------------------------
+select CityName from Application.Cities
+--избавимся от дублей
+select distinct CityName from Application.Cities
+
+
+--------------------------
+--ограничение вывода кол-ва строк (вся таблица не нужна)
+--top 10 - в каком порядке выведет информацию?
+select top 10 CityID, CityName, StateProvinceID from Application.Cities
+
+-- без сортировки порядок не гарантирован
+select top 10 CityID, CityName, StateProvinceID from Application.Cities order by CityName asc, StateProvinceID desc
+
+
+-- так будет работать?
+select top 10 CityID, CityName, StateProvinceID from Application.Cities order by 2, 3 desc
+-- pgdn
+
+
+
+
+
+--сортировка изменится при добавлении колонок => сортировать лучше не по номеру колонки
+select top 10 CityName, StateProvinceID, LastEditedBy from Application.Cities order by 2, 3 desc
+
+--порядок сортировки зависит от типа данных
+; with cte as (
+	select 1 as val --число
+	union all select 5
+	union all select 10
+)
+select * from cte order by val
+--все то же самое, но тип - строка
+--что получим при order by val?
+; with cte as (
+	select '1' as val --строка
+	union all select '5'
+	union all select '10'
+)
+select * from cte order by val
+
+
+
+
+
+
+
+-- вывести товары, входящие в 3ку самых дорогих
+SELECT TOP 10 StockItemID, StockItemName, UnitPrice FROM Warehouse.StockItems ORDER BY UnitPrice DESC
+
+
+
+
+
+
+
+
+
+
+SELECT TOP 3 StockItemID, StockItemName, UnitPrice FROM Warehouse.StockItems ORDER BY UnitPrice DESC
+
+SELECT TOP 3 WITH TIES StockItemID, StockItemName, UnitPrice FROM Warehouse.StockItems ORDER BY UnitPrice DESC
+
+-- -------------------------------
+-- SELECT TOP N WITH TIES ... - выводит N строк + все строки с граничным значением столбцов сортировки
+-- сортировка обязательна!!!
+-- -------------------------------
+
+------------------------------------------
+-- Постраничная выборка
+--сортировка обязательна 
+DECLARE 
+    @pagesize BIGINT = 20, -- Размер страницы
+    @pagenum  BIGINT = 1;  -- Номер страницы
+
+SELECT StockItemID, StockItemName, UnitPrice FROM Warehouse.StockItems ORDER BY UnitPrice DESC
+OFFSET (@pagenum - 1) * @pagesize ROWS FETCH NEXT @pagesize ROWS ONLY
---
+++ 02-select/demo/2-where.sql
@@ -0,0 +1,131 @@
+﻿use WideWorldImporters;
+
+-----------------------
+--where - фильтрация строк по условию
+--условие - логическое выражение
+--для каждой строки проверяем условие 
+--	true - строка попадает в выборку
+--	false - не попадет
+-----------------------
+-- логическое выражение: true, false и неопределено (если в поле null)
+select Size, * from Warehouse.StockItems where 0=1
+
+--227 строк
+select Size, * from Warehouse.StockItems
+
+--9 строк
+select Size, * from Warehouse.StockItems where Size = '1/12 scale'
+
+--154 строки
+select Size, * from Warehouse.StockItems where Size != '1/12 scale'
+
+select Size, * from Warehouse.StockItems where Size = null --логическое выражение
+
+-----------------------
+--обработка null
+--where: is null, is not null
+--колонки: isnull(), coalesce()
+
+select Size, * from Warehouse.StockItems where Size is null
+
+select Size, ColorId, isnull(Size, 0), coalesce(Size, ColorId, -1e6), * 
+from Warehouse.StockItems 
+where Size is null
+
+
+--isnull() vs coalesce
+;with cte as (
+	select 1 as val
+	union all select null
+)
+select val, isnull(val, 1.4) as [isnull], coalesce(val, 1.4) as [coalesce]
+from cte
+--isnull() - преобразование к типу 1го параметра 
+--coalesce() - тип данных сохраняется
+
+
+----------------
+--SARGable - предикаты (условия, позволяющие использовать индекс) - посмотреть индексы
+--план запроса!
+select StockItemID from Warehouse.StockItems where StockItemID = 10 --sargable - план запроса - index seek
+
+select StockItemID from Warehouse.StockItems where sqrt(StockItemID) = 10 --не sargable (функция) план запроса - index scan
+--по возможности - преобразовать в sargable sqrt(StockItemID) = 10 -> StockItemID = 10 * 10
+select StockItemID from Warehouse.StockItems where StockItemID = 100 --не sargable (функция) план запроса - index seek
+
+
+select StockItemID from Warehouse.StockItems where StockItemID between 10 and 20 --sargable поиск по индексу
+
+--like 
+select * from Warehouse.StockItems where StockItemName like 'USB%'
+select * from Warehouse.StockItems where StockItemName like '%USB%'
+
+
+-------------
+-- несколько условий
+-- условие - логическое выражение 
+-- операции с логическими выражениями AND, OR, NOT
+-------------
+-- Нужно вывести StockItems, где цена от 350 до 500 и
+-- название начинается с USB или Ride.
+
+select RecommendedRetailPrice, *
+from Warehouse.StockItems
+where
+    RecommendedRetailPrice between 350 and 500 --цена от 350 до 500
+	AND StockItemName like 'USB%' --название начинается с USB
+ 	OR StockItemName like 'Ride%' --название начинается с Ride
+
+-- почему попали строки с ценой < 350? 
+--pgdn
+
+
+
+
+
+
+
+--AND, OR - есть очередность выполнения - вначале все AND, затем OR
+--смена приоритета - скобки
+-- Нужно вывести StockItems, где цена от 350 до 500 и
+-- название начинается с USB или Ride.
+select RecommendedRetailPrice, *
+from Warehouse.StockItems
+where
+    RecommendedRetailPrice between 350 and 500 --цена от 350 до 500
+	and (StockItemName like 'USB%' --название начинается с USB
+    or StockItemName like 'Ride%') --название начинается с Ride
+
+
+--работа с датами
+--псевдонимы или алиасы
+declare @dt datetime = getdate()
+select year(@dt) as [Год]  
+	, [Месяц] = month(@dt)
+	, datepart(quarter, @dt) as [Квартал !]
+	, datename(month, @dt) as "Месяц "
+	, format(@dt, 'MMMM', 'ru-ru') as [Месяц Ru]
+	, convert(varchar, @dt, 104) as [Дата] 
+	, datetrunc(month, @dt) as begin_of_month
+	, eomonth(@dt) as end_of_month --SQL2022
+
+--дата в условии where - удобен формат 'yyyyMMdd'
+select OrderDate from Sales.Orders where OrderDate = '20150501'
+select OrderDate from Sales.Orders where OrderDate = '01.05.2015'
+
+--почему разное кол-во?
+
+
+
+
+
+select @@language
+--на уровне сеанса
+SET LANGUAGE 'French'
+select @@language;  --Français
+
+select format(getdate(),'d')
+
+SET LANGUAGE 'Russian'
+select format(getdate(),'d')
+
